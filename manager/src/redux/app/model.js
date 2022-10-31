38b6469da74a61d83d26ae0ab96574460cf58644
Mxtzz <mazw2016@hotmail.com>
2022-10-31 11:25:31 +0800

    格式化

--- manager/src/redux/app/model.js
+++ manager/src/redux/app/model.js
@@ -3,7 +3,7 @@ import { effects } from './effects';
 import { getCookie, addCookie } from '@/utils/cookies';
 
 const initState = {
-  sidebarStatus: getCookie('sidebarStatus') ? !!+getCookie('sidebarStatus') : true,
+  sidebarStatus: getCookie('sidebarStatus') ? !!+getCookie('sidebarStatus') : false,
   device: 'desktop',
 };
 
